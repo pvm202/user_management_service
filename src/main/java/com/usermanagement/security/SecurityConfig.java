095252de2725bf010daf3b2120e6829a5188ee76
pvm202 <prashantmahajan4197@gmail.com>
2024-06-08 13:50:42 +0530

    this is working code commit @prashant

--- src/main/java/com/usermanagement/security/SecurityConfig.java
+++ src/main/java/com/usermanagement/security/SecurityConfig.java
@@ -1,31 +1,44 @@
 package com.usermanagement.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.usermanagement.jwt.JwtAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-	 @Bean
-	    public BCryptPasswordEncoder passwordEncoder() {
-	        return new BCryptPasswordEncoder();
-	    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-	    @Bean
-	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	        http
-	            .authorizeHttpRequests(authorize -> authorize
-	                .requestMatchers("/user/**").permitAll().requestMatchers("/auth/**").permitAll()
-	                .anyRequest().authenticated()
-	            )
-	            .csrf(csrf -> csrf.disable()); // Disable CSRF for simplicity
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	@Autowired
+	private AuthenticationEntryPoint authenticationEntryPoint;
 
-	        return http.build();
-	    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		System.out.println("request came to filterchain successfully");
+		httpSecurity.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(authorize -> authorize.requestMatchers("user/addUser").permitAll().
+						requestMatchers("user/getAllUsers").hasRole("ADMIN")
+						.requestMatchers("auth/**").permitAll().anyRequest().authenticated())
+				.exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		// JWT authentication filter before the UsernamePasswordAuthenticationFilter
+		httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		return httpSecurity.build();
+	}
+
 }
